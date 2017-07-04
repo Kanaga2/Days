47a8b3c69ea7a03622c3745caaefd890018054f8
Kanaga2 <priyakanaga112@gmail.com>
2017-07-04 21:18:40 +0530

    Create Days.java

---
+++ Days.java
@@ -0,0 +1,11 @@
+# Days
+import java.io.*;
+import java.util.*;
+public class Days
+{
+public static int daysbetweenusingjoda(Date d1,Date d2)
+{
+    new LocalDate(d1.getTime());
+    new LocalDate(d2.getTime().getDays());
+    }
+    }
